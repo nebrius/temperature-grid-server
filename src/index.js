47fa48e981525af4fb46dcbca571f62daa282c2f
Bryan Hughes <bryan@theoreticalideations.com>
2017-05-10 17:31:44 -0700

    Wrote sensor code

--- src/index.js
+++ src/index.js
@@ -21,3 +21,51 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+const SAMPLING_RATE = 500;
+
+app.get('/state', (req, res) => {
+  res.send(state);
+});
+
+const state = {};
+
+io.on('connection', (socket) => {
+  console.log(`Sensor ${socket.id} connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`Sensor ${socket.id} disconnected`);
+    delete state[socket.id];
+  });
+
+  socket.on('register', (msg, ack) => {
+    console.log(`Registering sensor ${socket.id} as ${msg.name}`);
+    state[socket.id] = {
+      name: msg.name,
+      temperature: NaN
+    };
+    ack();
+  });
+
+  socket.on('update', (msg) => {
+    if (state.hasOwnProperty(socket.id)) {
+      state[socket.id].temperature = msg.temperature;
+    } else {
+      console.error(`Received update from unregistered sensor ${socket.id}`);
+    }
+  });
+
+});
+
+http.listen(3000, () => {
+  console.log('Temperature Grid server listening on port 3000');
+});
+
+setInterval(() => {
+  console.log(state);
+}, SAMPLING_RATE);
